caa314f963547792789fc786adcbbf4415171493
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-09-07 12:01:59 -0500

    feat: :tada: Initialize proyect
    Initialize program for default archives
    
    @duvanrg

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+
+/**
+el profesor del area de matematicas necesita un programa que le permita registrar los estudiantes que se encuentran matriculados, la informacion que el docente posee de cada estaudiante es la siguiente:
+-codigo del estudiante: con una logitud maxina de 15 caracteres
+-nombre del estudiante: con una logitud macima de 40 caracteres
+-correo electronico del estudiante: con una cantidad maxima de 40 caracteres
+-edad del estudiante: debe ser un entero y no puede ser negativo
+-direccion: con una longitud de 35 caracteres
+
+No se conoce la cantidad de estudiantes que se registraron en la asignatura.
+
+la universidad tiene como norma que cada estudiante debe presentar 4 quices 2 trabajos y 3 parciales
+
+la notas de los equivalen a:
+quices: 25%
+trabajos: 15%
+parciales: 60%
+
+el programa debe permitirle al profesor generar los siguiente reportes:
+1. listado general de notas del grupo paginado por 10 estudiantes
+2. listado con las definitivas de cada nota y la nota final
+el programa debe permitirle al docente hacer el ingreso de Quices, Trabajos y Parciales
+*/
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
