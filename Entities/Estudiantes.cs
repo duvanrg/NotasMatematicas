653a531ebd08c9817a95c7558277613f68990541
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-09-07 12:10:34 -0500

    feat: :sparkles: class Estudiantes
    Code basic of class Estudiantes
    
    @duvanrg

--- Entities/Estudiantes.cs
+++ Entities/Estudiantes.cs
@@ -8,6 +8,30 @@ namespace NotasMatematicas.Entities
 
     public class Estudiantes
     {
-        
+        private string id;
+        private string nombre;
+        private string email;
+        private int edad;
+        private string direccion;
+
+        public Estudiantes()
+        {
+        }
+
+        public Estudiantes(string id, string nombre, string email, int edad, string direccion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.email = email;
+            this.edad = edad;
+            this.Direccion = direccion;
+        }
+
+        public string Id { get { return id; } set { id = value; } }
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public string Email { get { return email; } set { email = value; } }
+        public int Edad { get { return edad; } set { edad = value; } }
+        public string Direccion { get { return direccion; } set { direccion = value; } }
+
     }
 }
